98a025b131a119fd8bc54aaf3b6ef288589b83cc
hyowon <kanghw1103@naver.com>
2025-06-09 01:16:06 +0900

    postgres db로 변경

--- wantedlab/settings.py
+++ wantedlab/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "wantedlab.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "wantedlab",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
